b587629a3b02c7c541f49d5c4be4ab1af99dc2c0
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2024-11-11 23:19:28 +0200

    adding fourteenth classwork cpp files not exe

---
+++ CW14_1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    char fname[] = "test4.txt";
+    fstream f(fname);
+    if (!f) {
+        cerr << "Error reading file: " << fname << "\n";
+        return -1;
+    }
+
+    char c;
+    int num = 0;
+    int num2 = 0;
+    int summ = 0;
+    int prev_sign = 0; // 0: no sign, 1: +, 2: -
+
+    while (f >> c) {
+        if (isdigit(c)) {
+            int d = c - '0';
+            num = num * 10 + d;
+        } 
+        else if (c == '-') {
+            num2 = num;
+            num = 0;
+            if (prev_sign == 1 || prev_sign == 0) {
+                summ += num2;
+            } 
+            else if (prev_sign == 2) {
+                summ -= num2;
+            }
+            prev_sign = 2;
+            cout << summ << "; ";
+        } 
+        else if (c == '+') {
+            num2 = num;
+            num = 0;
+            if (prev_sign == 1 || prev_sign == 0) {
+                summ += num2;
+            } 
+            else if (prev_sign == 2) {
+                summ -= num2;
+            }
+            prev_sign = 1;
+            cout << summ << "; ";
+        } 
+        else if (c == '=') {
+            num2 = num;
+            if (prev_sign == 1) {
+                summ += num2;
+            } 
+            else if (prev_sign == 2) {
+                summ -= num2;
+            }
+            cout << "sum=" << summ << "\n";
+        } 
+        else {
+            cout << "Not required format: " << c << " - incorrect symbol\n";
+        }
+    }
+}
---
+++ CW14_4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+bool checkTags(ifstream& f1, const string& openTD = "<td>", const string& closeTD = "</td>") {
+    int opentag = 0;
+    int closetag = 0;
+    string buf;
+    while(!f1.eof()) {
+        if(f1 >> buf) {
+            if(buf == openTD) {
+                opentag ++;
+            }
+            else if(buf == closeTD) {
+                closetag ++;
+            }
+            if(closetag > opentag) {
+                return false;
+            }
+            else {
+                break;
+            }
+        }
+           
+    }
+    return closetag == opentag; 
+}
+
+int main() {
+    const char* fileName = "file.txt";
+    ifstream f1(fileName);
+    if(f1.bad()) {
+        cerr << "Error opening file " << fileName << endl;
+    }
+    cout << "Check" << boolalpha << checkTags(f1);
+    f1.close();
+}
