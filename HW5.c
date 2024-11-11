335ed68375f43f649b011aa66f56360c1dc14d14
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2024-11-12 00:01:54 +0200

    adding fixed fifth homework

--- HW5.c
+++ HW5.c
@@ -2,49 +2,51 @@
 #include <math.h>
 #include <float.h>
 
-double eps = 0.001;
+#define EPS 1e-6 
 
 
 void task5_13b() {
-    double a1, b1, ak = 0, bk = 0, Pn, mul = 0;
+    double a1 = 1, b1 = 1, ak = 0, bk = 0, Pn = 1, mul = 0;
     int n;
-    a1 = 1;
-    b1 = 1;
-    Pn = 1;
+
     printf("n = ");
     scanf("%d", &n);
+
     for (int i = 0; i < n; i++) {
-        ak = (sqrt(b1) + a1) / 5;
-        bk = (2 * b1) + (5 * pow(a1, 2));
+        ak = (sqrt(b1) + a1) / 5.0;
+        bk = (2.0 * b1) + (5.0 * a1 * a1);
+        if (bk > 1e10) {
+            printf("bk has grown too large at iteration %d\n", i + 1);
+            break;
+        }
+
         mul = ak * bk;
-        printf("iteration %d: ak = %lf, bk = %lf, mul = %lf\n", i+1, ak, bk, mul);
+        printf("iteration %d: ak = %lf, bk = %lf, mul = %lf\n", i + 1, ak, bk, mul);
         Pn *= mul;
         a1 = ak;
         b1 = bk;
     }
+    
     printf("p = %lf\n", Pn);
 }
 
 
-int factorial(int n) {
-    if (n == 0) return 1;
-    int result = 1;
-    for (int i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
-}
+ 
 
 void task5_17a() {
     int k = 0;
-    double x, x_new, sum = 0;
+    double x, x_new, sum = 0, factorial = 1, x_power = 1;
     printf("x = ");
     scanf("%lf", &x);
+
     do {
-        x_new = fabs(pow(x, 2 * k) / (2 * factorial(k)));
+        x_new = fabs(x_power / (2 * factorial));
+
         sum += x_new;
         k++;
-    } while (x_new > eps);
+        x_power *= x * x;             
+        factorial *= k;               
+    } while (x_new > EPS);
 
     printf("sum = %lf\n", sum);
 }
@@ -52,6 +54,5 @@ void task5_17a() {
 
 int main() {
     // task5_13b();
-    // task5_17a();
+    task5_17a();
 }
-
