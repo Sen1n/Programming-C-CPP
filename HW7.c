1e191e00f3bd22efcf7026e72f5488cfdf1e2769
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2024-11-12 02:51:06 +0200

    adding fixed 7 homework

--- HW7.c
+++ HW7.c
@@ -1,73 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void task7_9v() {
     unsigned n;
     printf("n = ");
     scanf("%u", &n);
-    double arr[n], max;
-    for (int i = 1; i < n; i+=2) {
-        //почав індексацію з 1, щоб додававши 2, проходив по елементам з парним індексом
-        // 0 індекс відповідає а1 згідно з умовою, 1->2, ...
+
+    double* arr = (double*)malloc(n * sizeof(double));
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    for (unsigned i = 0; i < n; i++) {
+        printf("arr[%d] = ", i);
         scanf("%lf", &arr[i]);
-        printf("arr[%d] = %lf\n", i, arr[i]);
     }
-    max = arr[1];
-        for (int j = 1; j < n; j+=2) {
-            if (arr[j] > max) {
-                max = arr[j];
-            }
+
+    double max = arr[1];
+    for (unsigned i = 1; i < n; i += 2) { 
+        if (arr[i] > max) {
+            max = arr[i];
+        }
     }
-    printf("maximum = %lf\n", max);
+
+    printf("max = %lf\n", max);
+    free(arr);
 }
 
 
 void task7_17() {
+    unsigned n, counter_of_marks = 0;
     double mark = 0, average;
-    unsigned n, counter_of_marks = 0, cur_max, cur_min, length;
+
     printf("n = ");
     scanf("%u", &n);
-    double arr[n], max;
+    double* arr = (double*)malloc(n * sizeof(double));
+    if (arr == NULL) {
+        printf("memory allocation failed.\n");
+        return;
+    }
+
     for (int i = 0; i < n; i++) {
         printf("arr[%d] = ", i);
         scanf("%lf", &arr[i]);
-        printf("arr[%d] = %lf", i, arr[i]);
-        printf("\n");
         mark += arr[i];
         counter_of_marks++;
     }
-    // length = sizeof(arr) / sizeof(arr[0]);
-    // printf("mark = %lf\n", mark);
-    cur_max = arr[0];
-    cur_min = arr[0];
+    double cur_max = arr[0], cur_min = arr[0];
     for (int i = 1; i < n; i++) {
-        if (arr[i] > cur_max) {
-            cur_max = arr[i];
-        }
-        if (arr[i] < cur_min) {
-            cur_min = arr[i];
-        }
+        if (arr[i] > cur_max) cur_max = arr[i];
+        if (arr[i] < cur_min) cur_min = arr[i];
     }
     for (int i = 0; i < n; i++) {
         if (arr[i] == cur_max && counter_of_marks > 0) {
             mark -= arr[i];
             counter_of_marks--;
-
+            cur_max = -1; 
         }
         if (arr[i] == cur_min && counter_of_marks > 0) {
             mark -= arr[i];
             counter_of_marks--;
+            cur_min = -1;  
         }
     }
+
     if (counter_of_marks > 0) {
         average = mark / counter_of_marks;
         printf("counter_of_marks = %u\n", counter_of_marks);
         printf("mark = %lf\n", mark);
         printf("average = %lf\n", average);
+    } else {
+        printf("no valid marks left to calculate an average.\n");
     }
-
+    
+    free(arr);
 }
-
-
 
 int main() {
     // task7_9v();
